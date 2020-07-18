3a212daf56e6a6cdf69a3f66531b6e6293f47de5
zingimmick <zingimmick@outlook.com>
2020-07-19 07:56:02 +0800

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 set -e
-wget https://github.com/rectorphp/rector-prefixed/raw/v$1/rector.phar -O rector \
+wget https://github.com/rectorphp/rector-prefixed/raw/$1/rector.phar -O rector \
     && chmod a+x rector \
     && mv rector /usr/local/bin/rector
 /usr/local/bin/rector process $2
